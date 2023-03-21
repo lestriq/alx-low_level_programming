c185f37493b23d630f03b949fe3d02e3081b476a
lestriq <lestriqvibes @gmail.com>
2023-03-21 09:06:41 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program showing how to use Printf
+ *
+ * Description: A C program that prints with printf function
+ *
+ * return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
