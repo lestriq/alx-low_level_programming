cae907c444eddb5da3561bbbb9eee3335f47b83e
lestriq <lestriqvibes @gmail.com>
2023-03-22 16:06:06 -0700

    Letters Reversed order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function for this program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	int x;
+
+	for x (x = z ; x >=a ; x++)
+		printf("x");
+	return; (0);
