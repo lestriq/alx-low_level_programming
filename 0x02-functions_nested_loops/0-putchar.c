869e3392790360be890009d89178a49773716f61
lestriq <lestriqvibes @gmail.com>
2023-03-23 07:40:13 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main function od this program
+ *
+ * Description: putchar funtion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file for Nested loops Project
