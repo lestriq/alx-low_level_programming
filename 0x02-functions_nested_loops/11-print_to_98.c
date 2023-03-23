5a83c0150279e8a6136c967a57a0baf1f1645214
lestriq <lestriqvibes @gmail.com>
2023-03-23 16:28:19 -0700

    98 Battery str

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,20 @@
-#include"main.h"
+#include "main.h"
+   /**
+   * jack_bauer - prints every minute of the day 
+   *              from 00:00 to 23:59
+   */
+    void jack_bauer(void) 
+  {  
+     int hour, minute;
 
-/**
- * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
- *
- * @n: input
-*/
-
-void print_to_98(int n)
-{
-	int count;
-
-	if (n > 98)
-		for (count = n; count > 98; --count)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; ++count)
-			printf("%d, ", count);
-	printf("98\n");
-}
-
+   for (hour = 0; hour <= 23; hour ++) 
+        {
+           for (minute = 0; minute <= 59; minute++)
+ { 
+          _putchar ((hour / 10) + ('0');
+          _putchar ((hour % 10) + ('0');
+          _putchar (':');
+          _putchar ((minute / 10) + ('0');
+          _putchar ((minute % 10) + ('0');
+          _putchar ('\n');
+   } 
