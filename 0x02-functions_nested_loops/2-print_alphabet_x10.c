a570fe2cbd413764826289c4cc484896d0ac1863
lestriq <lestriqvibes @gmail.com>
2023-03-23 08:45:57 -0700

    Alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,16 @@
 
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *
- * Return: 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int j;
 	char j;
 
-	while (i = 'a' ; i <= 'z' ; i++)
+	while (i++ <= 9)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
-
 }
