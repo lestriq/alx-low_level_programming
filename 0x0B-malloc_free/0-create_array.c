7636761f824076a2d7797f993418dd8ae9917f7c
lestriq <lestriqvibes @gmail.com>
2023-04-20 09:42:23 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -31,15 +31,10 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 		
 	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
 
-		
+		return (NULL);	
 	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
+
+		str[i] = c;		
+	return (str);		
 }
